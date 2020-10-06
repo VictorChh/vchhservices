f8b33227f20dce535d946d44e0641edd080b891c
victorchh <victor.chh@hotmail.com>
2020-10-06 16:59:35 -0400

    change routes

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var app = express();
 //Setings
 app.set('view engine', 'ejs');
 app.set('port', process.env.PORT || 3000)
+app.set('views', './views/routes/');
 
 
 //Routs
@@ -14,20 +15,20 @@ app.get('/', function(req, res)
 
 app.get('/about', function(req, res) 
 {
-    res.render('views/routes/about.ejs')
+    res.render('about.ejs')
 });
 
 app.get('/projects', function(req, res) 
 {
-    res.render('/routes/projects.ejs')
+    res.render('projects.ejs')
 });
 app.get('/services', function(req, res) 
 {
-    res.render('/routes/contact.ejs')
+    res.render('contact.ejs')
 });
 app.get('/contact', function(req, res) 
 {
-    res.render('/routes/contact.ejs')
+    res.render('contact.ejs')
 });
 
 
