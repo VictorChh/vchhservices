bfa6574336ba1f711730e53a0c1f930443718f26
victorchh <victor.chh@hotmail.com>
2020-10-06 15:54:06 -0400

    file structure changes

--- index.js
+++ index.js
@@ -14,14 +14,20 @@ app.get('/', function(req, res)
 
 app.get('/about', function(req, res) 
 {
-    var message="<a href='/projects'>Projects</a>";
-    res.send(message);
+    res.render('views/routes/about.ejs')
 });
-
 
 app.get('/projects', function(req, res) 
 {
-    res.send('Estos son mis projectos Projects');
+    res.render('/routes/projects.ejs')
+});
+app.get('/services', function(req, res) 
+{
+    res.render('/routes/contact.ejs')
+});
+app.get('/contact', function(req, res) 
+{
+    res.render('/routes/contact.ejs')
 });
 
 
