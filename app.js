cc3595e34fb3c293398c64c257912154b868eff8
victorchh <victor.chh@hotmail.com>
2020-10-09 17:19:46 -0400

    changes in app.js

--- app.js
+++ app.js
@@ -2,13 +2,17 @@
 let createError = require('http-errors');
 let express = require('express');
 let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
 let indexRouter = require('./routes/index');
+
 let app = express();
 
 // EJS views settings
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+//app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
