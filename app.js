0ca322f4474dc22feb7ad5388c8f64c789bccc39
victorchh <victor.chh@hotmail.com>
2020-10-08 21:20:21 -0400

    changes in app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 
 let indexRouter = require('./routes/index');
-let usersRouter = require('./routes/users');
+//let usersRouter = require('./routes/users');
 
 let app = express();
 
@@ -20,9 +20,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
-
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,11 @@
+/* 
+* Index js
+* Created by Victor Chaparro
+* Studen ID: 301140944
+* Oct - 05 - 2020
+* This js file manage all the different routes
+*/
+
 var express = require('express');
 var router = express.Router();
 
@@ -8,27 +16,27 @@ router.get('/', function(req, res) {
 
 /* GET home page. */
 router.get('/home', function(req, res, next) {
-  res.render('index', { title: 'Home'});
+  res.render('index');
 });
 
 /* GET About Us page. */
 router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'About'});
+  res.render('about');
 });
 
 /* GET Products page. */
 router.get('/services', function(req, res, next) {
-  res.render('services', { title: 'Services'});
+  res.render('services');
 });
 
 /* GET Services page. */
 router.get('/projects', function(req, res, next) {
-  res.render('projects', { title: 'Services'});
+  res.render('projects');
 });
 
 /* GET Contact Us page. */
 router.get('/contact', function(req, res, next) {
-  res.render('contact', { title: 'Contact'});
+  res.render('contact');
 });
 
 module.exports = router;
