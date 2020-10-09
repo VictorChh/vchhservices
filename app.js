21741c523a2baa3b3bc4b081725a9464f93bd44d
victorchh <victor.chh@hotmail.com>
2020-10-09 17:17:20 -0400

    changes in app.js

--- app.js
+++ app.js
@@ -1,29 +1,22 @@
-// installed 3rd party packages
+// package definitions
 let createError = require('http-errors');
 let express = require('express');
 let path = require('path');
-let cookieParser = require('cookie-parser');
-let logger = require('morgan');
-
 let indexRouter = require('./routes/index');
-//let usersRouter = require('./routes/users');
-
 let app = express();
 
-// view engine setup
+// EJS views settings
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs'); // express  -e
+app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
 app.use('/', indexRouter);
-//app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+// function to cath 404 error
 app.use(function(req, res, next) {
   next(createError(404));
 });
